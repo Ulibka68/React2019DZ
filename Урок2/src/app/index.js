161a40d4648b2fb3b1e06e8d65e83d7e04bdd9f4
Gayrat Vlasov <33682052+Ulibka68@users.noreply.github.com>
2019-11-01 23:53:03 +0300

    les 2 test класса

--- Урок2/src/app/index.js
+++ Урок2/src/app/index.js
@@ -1,4 +1,11 @@
 class App {
+
+    constructor(a) {
+        this.a = a;
+    }
+
+    static static_prop = 220;
+
     state = {
         showModal: false
     }
--- Урок2/src/main.js
+++ Урок2/src/main.js
@@ -1,4 +1,6 @@
 import App from './app';
 
-let a = new App();
+let a = new App(22);
 a.show();
+console.log("a.a : ",a.a);
+console.log("App.static_prop : ",App.static_prop);
