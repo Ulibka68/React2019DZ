f114bdc26d3f91e2763d3798df0c4f67fe0621a1
Ulibka <gena68@inbox.ru>
2019-11-05 13:43:16 +0300

    initial commit

---
+++ les4/src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app-header.css";
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Гайрат Власов</h1>
+            <h2>Записей 5, из них понравилось 1</h2>
+        </div>
+    );
+}
+export default AppHeader;
