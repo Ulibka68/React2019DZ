d78f997d581d717c00f0fb0cc5ba62d63b35a64f
Gayrat Vlasov <33682052+Ulibka68@users.noreply.github.com>
2019-11-15 00:03:33 +0300

    HOC работает

---
+++ hoc/src/hoc.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+ function Elna () {
+    return (
+        <div>
+            <h1>Петя</h1>
+        </div>
+    )
+}
+
+const f= () => {
+    return Elna;
+}
+
+export default f;
