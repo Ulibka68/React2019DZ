a3eadfc572ba69768c781d72e49cfeaf543bac56
Ulibka <gena68@inbox.ru>
2019-11-19 16:45:48 +0300

    ДЗ 13

--- les13/src/index.js
+++ les13/src/index.js
@@ -1,1 +1,53 @@
-console.log('Hello');
+import {createStore} from "redux";
+
+
+const reducer = (state = 0,action) => {
+    switch ( action.type ) {
+        case 'INC' :
+            return state+1;
+        case 'DEC' :
+            return state-1;
+        case 'RND' :
+            return action.value;
+        case 'RESET' :
+            return 0;
+        default:
+            return state;
+    }
+}
+
+
+const inc = () => ({type : 'INC'});
+const dec = () => ({type : 'DEC'});
+const rnd = (value) => ({type : 'RND',value});
+
+
+
+const store = createStore(reducer);
+
+const update = () => {
+    document.getElementById( "counter").textContent = store.getState();
+}
+
+store.subscribe(update);
+
+document.getElementById('inc').addEventListener('click',() => {
+    store.dispatch({type: 'INC'});
+});
+document.getElementById('dec').addEventListener('click',() => {
+    store.dispatch( dec());
+});
+document.getElementById('rnd').addEventListener('click',() => {
+    // const val = Math.floor(Math.random()*100);
+    store.dispatch({type : 'RESET'});
+});
+
+//         store.subscribe(()=>{
+//             console.log('SUB : ',store.getState());
+//         });
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+
+
