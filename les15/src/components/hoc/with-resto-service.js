7657b7646b4fcfeb31c6de7eeae01e48f1fd4b42
Ulibka <gena68@inbox.ru>
2019-11-21 13:24:35 +0300

    10:39

--- les15/src/components/hoc/with-resto-service.js
+++ les15/src/components/hoc/with-resto-service.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import RestoServiceContext from '../resto-service-context';
 
-const WithRestoService = () => () => {
-    return 1;
-};
+const WithRestoService = () => 
+    (Wrapped) => {
+        return (props) => {
+            return (
+                <RestoServiceContext.Consumer>
+                    {
+                        // {  Consumer принимает функцию в качестве дочернего компонента. 
+                        //  Эта функция принимает текущее значение контекста и возвращает React-компонент }
+                        (RestoService) => {
+                            return <Wrapped {...props} RestoService={RestoService} />
+                        }
+                    }
+                </RestoServiceContext.Consumer>
+            )
+        }
+    };
 
 export default WithRestoService;
