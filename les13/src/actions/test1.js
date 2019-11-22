b3f1e326961158930879fe5757be6786ef588ff9
Ulibka <gena68@inbox.ru>
2019-11-22 12:41:17 +0300

    вар 2

--- les13/src/actions/test1.js
+++ les13/src/actions/test1.js
@@ -1,14 +1,17 @@
 // import reducer from "../store/reducer";
 import {inc, dec, reset, rnd} from "./actions"
 import { store } from "../store/store";
+import {bindActionCreators} from "redux";
 
 
 // Цепочка действий - создать и экспортировать action
 
 // код store.dispacth(inc()); можно оптимизировать
+/*
 export const incDispatch = () => store.dispacth(inc());
 export const decDispatch = () => store.dispacth(dec());
 export const rndDispatch = (value) => store.dispacth(rnd(value));
+*/
 
 // при клике вызывает соответсвующий action
 document.getElementById('inc').addEventListener('click',() => {
@@ -23,3 +26,22 @@ document.getElementById('inc').addEventListener('click',() => {
     const val =56;
     rndDispatch(val);
 });
+
+
+// Вариант2
+
+const bindActionCreator = (creator, dispatch) => (...args) => {
+    dispatch(creator(...args));
+}
+
+// код store.dispacth(inc()); можно оптимизировать
+/*
+export const incDispatch = bindActionCreator(inc,store.dispacth);
+export const rndDispatch = bindActionCreator(inc,store.dispacth);
+*/
+
+// Варинат 3 
+// функция  bindActionCreator уже есть в Redux
+// bindActionCreators - уже из redax
+export const incDispatch = bindActionCreators(inc,store.dispacth);
+export const rndDispatch = bindActionCreators(inc,store.dispacth);
